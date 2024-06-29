493c839b45526a7d8011219a6626f4efc1d573b7
Felipe de Freitas Borges <felipedefreitasborges@gmail.com>
2024-06-29 20:57:15 -0300

    Pag-Login renomeada para Pag-Registro e criada pag-login com estilizaçao correta

---
+++ pag-login/Login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Login.css">
+    <title>Registro</title>
+</head>
+<body>
+    <div class="container">
+        <div class="form-image">
+            <img src="./assets/image-registerpag.svg" alt="">
+        </div>
+        <div class="form">
+            <form action="Registro.php" method="POST">
+                <div class="form-header">
+                    <div class="title">
+                        <h1>Entre em sua conta!</h1>
+                    </div>
+                </div>
+                <div class="input-group">
+                    <div class="input-box">
+                        <label for="email">E-mail:</label>
+                        <input id="email" type="email" name="email" placeholder="seuemail@email.com" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="password">Sua senha:</label>
+                        <input id="password" type="password" name="password" placeholder="Digite sua senha:" required>
+                    </div>
+                </div>
+                <div id="errorParagraph"><p id="passwordError" style="color: red;"></p></div>
+                <div class="gender-inputs">
+                </div>
+                <div class="continue-button">
+                    <Button type="submit" name="submit">Continua</Button>
+                </div>
+                <div class="likToRegistro">
+                    <p>Não tem uma conta? <a href="#">Cadastre-se!</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+<script src="script/validator.js"></script>
+</html>
